ade3bc345568944828a8a06a3e99218d3bae8f13
nirvi-parikh <nirvi.parikh@gmail.com>
2018-11-02 22:23:05 -0400

    Binomial Distribution I

---
+++ Binomial_DistributionI.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  2 15:56:04 2018
+
+@author: nirvi
+"""
+def fact(n):
+    return 1 if n == 0 else n*fact(n-1)
+
+def comb(n, x):
+    return fact(n) / (fact(x) * fact(n-x))
+
+def b(x, n, p):
+    return comb(n, x) * p**x * (1-p)**(n-x)
+
+l, r = list(map(float, input().split(" ")))
+odds = l / r
+print(round(sum([b(i, 6, odds / (1 + odds)) for i in range(3, 7)]), 3))
